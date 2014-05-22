9e195a79103e91b59b89d60e9e9847bc34602c14
akira <h3.poteto@gmail.com>
2014-05-22 23:45:01 +0900

    カラータグ登録機能追加

--- MenuRecords2/InputViewController.h
+++ MenuRecords2/InputViewController.h
@@ -5,28 +5,45 @@
 //  Created by akirafukushima on 2014/05/13.
 //  Copyright (c) 2014年 AkiraFukushima. All rights reserved.
 //
+#define TOP_SPAN    90
+#define FIELD_SPAN  60
+#define BOTTOM_SPAN 80
+#define ACVIEW_PADDING  30
+#define ACVIEW_HEIGHT   320
+#define ACVIEW_TOP      70
+#define COLOR_HEIGHT    30
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
 #import "FMDatabase.h"
 
-@interface InputViewController : UIViewController <UITextFieldDelegate>{
-    NSString *db_path;
-    UIActionSheet *basicSheet;
-    UIScrollView *scrollView;
-    NSInteger menu_index;
-    NSArray *textField_array;
+@interface InputViewController : UIViewController <UITextFieldDelegate, UIActionSheetDelegate>{
+    NSString        *db_path;
+    UIActionSheet   *basicSheet;
+    UIActionSheet   *colorSheet;
+    UIScrollView    *scrollView;
+    NSInteger       menu_index;
+    NSArray         *textField_array;
+    NSString        *saveStr; //　一時保保管
+    UIColor         *saveColor; // 一時保管
+    NSInteger       tagColorbutton;
 }
-@property (strong, nonatomic) IBOutlet UIButton *recordDate;
-@property (strong, nonatomic) NSDate *date_value;
-@property (strong, nonatomic) IBOutlet UIButton *addMenuButton;
-@property (strong, nonatomic) IBOutlet UIButton *submitButton; // 要create
-- (void)Submit:(id)sender;  // actionに変更し動的呼び出し
+@property (weak, nonatomic) IBOutlet UIButton   *recordDate;
+@property (strong, nonatomic) NSDate            *date_value; // date_value設定値
+@property (weak, nonatomic) IBOutlet UIButton   *addMenuButton;
+@property (weak, nonatomic) IBOutlet UIButton   *submitButton;
+@property (strong, nonatomic) IBOutlet UIButton *colorTagButton;
+
+
+- (void)Submit:(id)sender;
 - (void)touchDate:(id)sender;
 - (void)getSelectedTime;
 - (void)dismissSet;
 - (void)cancelSet;
 - (void)button_Tupped:(id)sender;
-
-
+- (void)touchColorTagButton:(id)sender;
+- (void)colorSet;
+- (void)colorCancelSet;
+- (void)touchColor:(UIButton *)button;
 
 @end
--- MenuRecords2/InputViewController.m
+++ MenuRecords2/InputViewController.m
@@ -13,7 +13,7 @@
 @end
 
 @implementation InputViewController{
-    NSString *saveStr; //　一時保保管
+
 }
 
 @synthesize recordDate, date_value;
@@ -27,17 +27,21 @@
     return self;
 }
 
+//==============================
+// load function
+//==============================
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
     //DB confirm
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES);
-    NSString *dir = [paths objectAtIndex:0];
+    NSArray     *paths = NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES);
+    NSString    *dir = [paths objectAtIndex:0];
     db_path = [dir stringByAppendingPathComponent:@"menu_records.db"];
     //NSLog(@"%@", db_path);
-    FMDatabase *db = [FMDatabase databaseWithPath:db_path];
-    NSString *sql = @"CREATE TABLE IF NOT EXISTS menulogs (id INTEGER PRIMARY KEY AUTOINCREMENT, parent_id INTEGER, name TEXT, date REAL);";
+    FMDatabase  *db = [FMDatabase databaseWithPath:db_path];
+    NSString    *sql = @"CREATE TABLE IF NOT EXISTS menulogs (id INTEGER PRIMARY KEY AUTOINCREMENT, parent_id INTEGER, name TEXT, color_tag TEXT, date REAL);";
     [db open];
     [db executeUpdate:sql];
     [db close];
@@ -55,17 +59,17 @@
     
     // init recordDate
     date_value = [NSDate date];
-    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
-    NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ja_JP"];
+    NSDateFormatter     *fmt = [[NSDateFormatter alloc] init];
+    NSLocale    *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ja_JP"];
     [fmt setLocale:locale];
     [fmt setCalendar:[[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar]];
     [fmt setDateFormat:@"yyyy/MM/dd"];
-    NSString *string_date = [fmt stringFromDate:date_value];
+    NSString    *string_date = [fmt stringFromDate:date_value];
     
     
     // 日付ラベル描画
-    CGRect date_label_rect = CGRectMake(20, 90, 100, 30);
-    UILabel *date_label = [[UILabel alloc] initWithFrame:date_label_rect];
+    CGRect  date_label_rect = CGRectMake(20, TOP_SPAN, 100, 30);
+    UILabel     *date_label = [[UILabel alloc] initWithFrame:date_label_rect];
     date_label.textColor = [UIColor blackColor];
     date_label.font = [UIFont fontWithName:@"HiraKakuProN-W3" size:13];
     date_label.textAlignment = NSTextAlignmentLeft;
@@ -74,7 +78,7 @@
     [scrollView addSubview:date_label];
     
     // 日付ボタン描画
-    UIButton *date_button = [[UIButton alloc] initWithFrame:CGRectMake(100, 90, 200, 30)];
+    UIButton    *date_button = [[UIButton alloc] initWithFrame:CGRectMake(100, TOP_SPAN, 200, 30)];
     [date_button setTitle:string_date forState:UIControlStateNormal];
     [date_button setFont:[UIFont fontWithName:@"HiraKakuProN-W3" size:14]];
     [date_button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
@@ -83,10 +87,28 @@
     [scrollView addSubview:date_button];
     self.recordDate = date_button;
     
+    // カラータグ
+    // カラータグラベル描画
+    CGRect      color_label_rect = CGRectMake(20, TOP_SPAN + FIELD_SPAN, 100, 30);
+    UILabel     *color_tag_label = [[UILabel alloc] initWithFrame:color_label_rect];
+    color_tag_label.textColor = [UIColor blackColor];
+    color_tag_label.font = [UIFont fontWithName:@"HiraKakuProN-W3" size:13];
+    color_tag_label.textAlignment = NSTextAlignmentLeft;
+    color_tag_label.text = @"カラータグ";
+    [scrollView addSubview:color_tag_label];
+    
+    //カラータグボタン描画
+    self.colorTagButton = [[UIButton alloc] initWithFrame:CGRectMake(150, TOP_SPAN + FIELD_SPAN, 100, 30)];
+    [self.colorTagButton setBackgroundColor:[UIColor blueColor]];
+    [self.colorTagButton addTarget:self action:@selector(touchColorTagButton:) forControlEvents:UIControlEventTouchUpInside];
+    [scrollView addSubview:self.colorTagButton];
+    
+    saveColor = self.colorTagButton.backgroundColor;
+    tagColorbutton = 10;
     
     // 初期状態のメニューtextField生成
-    CGRect rect = CGRectMake(100, 150, 200, 30);
-    UITextField *customTextField = [[UITextField alloc] initWithFrame:rect];
+    CGRect          rect = CGRectMake(100, TOP_SPAN + FIELD_SPAN * 2, 200, 30);
+    UITextField     *customTextField = [[UITextField alloc] initWithFrame:rect];
     
     customTextField.borderStyle = UITextBorderStyleRoundedRect;
     customTextField.textAlignment = NSTextAlignmentLeft;
@@ -101,8 +123,8 @@
     textField_array = [NSArray arrayWithObjects:customTextField, nil];
     
     // メニューtextFieldのlabel生成
-    CGRect label_rect = CGRectMake(20, 150, 100, 30);
-    UILabel *label = [[UILabel alloc] initWithFrame:label_rect];
+    CGRect  label_rect = CGRectMake(20, TOP_SPAN + FIELD_SPAN * 2, 100, 30);
+    UILabel     *label = [[UILabel alloc] initWithFrame:label_rect];
     label.textColor = [UIColor blackColor];
     label.font = [UIFont fontWithName:@"HiraKakuProN-W3" size:13];
     label.textAlignment = NSTextAlignmentLeft;
@@ -111,9 +133,9 @@
     [scrollView addSubview:label];
     
     // 初期状態のメニューtextFieldの追加ボタン生成
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeContactAdd];
-    CGRect windowSize = [[UIScreen mainScreen] bounds];
-    button.center = CGPointMake(windowSize.size.width / 2.0, 225);
+    UIButton    *button = [UIButton buttonWithType:UIButtonTypeContactAdd];
+    CGRect      windowSize = [[UIScreen mainScreen] bounds];
+    button.center = CGPointMake(windowSize.size.width / 2.0, 285);
     [button addTarget:self action:@selector(button_Tupped:) forControlEvents:UIControlEventTouchUpInside];
     [scrollView addSubview:button];
     self.addMenuButton = button;
@@ -122,8 +144,8 @@
     menu_index = 1;
     
     // submitボタン
-    UIButton *submit_button = [[UIButton alloc] initWithFrame:CGRectMake(400, 100, 100, 30)];
-    submit_button.center = CGPointMake(windowSize.size.width / 2.0, windowSize.size.height - 80);
+    UIButton    *submit_button = [[UIButton alloc] initWithFrame:CGRectMake(400, 100, 100, 30)];
+    submit_button.center = CGPointMake(windowSize.size.width / 2.0, windowSize.size.height - BOTTOM_SPAN);
     [submit_button setTitle:@"登録" forState:UIControlStateNormal];
     [submit_button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [submit_button setBackgroundColor:[UIColor lightGrayColor]];
@@ -150,10 +172,10 @@
 }
 */
 
-/**
- return が押されたときにキーボードを隠す
- textField : Editしていたtextfield
- **/
+//========================================
+//  return が押されたときにキーボードを隠す
+//  textField : Editしていたtextfield
+//========================================
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     [self.navigationController.view endEditing:YES];
@@ -161,27 +183,36 @@
 }
 
 
-/**
- 登録ボタンが押された際のイベント
- **/
+//=====================================
+//  登録ボタンが押された際のイベント
+//=====================================
 - (void)Submit:(id)sender {
     NSInteger parent_id = -1;
     
-    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
-    NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ja_JP"];
+    NSDateFormatter     *fmt = [[NSDateFormatter alloc] init];
+    NSLocale            *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ja_JP"];
     [fmt setLocale:locale];
     [fmt setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"JST"]];
     [fmt setCalendar:[[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar]];
     [fmt setDateFormat:@"yyyy-MM-dd hh:mm:ss"];
-    NSString *string_date = [fmt stringFromDate:date_value];
+    NSString    *string_date = [fmt stringFromDate:date_value];
     
-    FMDatabase *db = [FMDatabase databaseWithPath:db_path];
+    FMDatabase  *db = [FMDatabase databaseWithPath:db_path];
+    
+    // カラータグ
+    CGFloat tag_red;
+    CGFloat tag_green;
+    CGFloat tag_blue;
+    CGFloat tag_alpha;
+    [self.colorTagButton.backgroundColor getRed:&tag_red green:&tag_green blue:&tag_blue alpha:&tag_alpha];
+    NSString *string_hex = [NSString stringWithFormat:@"%.2X%.2X%.2X", (int)(tag_red * 255), (int)(tag_green * 255), (int)(tag_blue * 255)];
+    
     [db open];
     
     //index
-    int index = 0;
+    int     index = 0;
     for ( UITextField *textField in textField_array ){
-        NSString *insert_sql = [[NSString alloc] initWithFormat: @"INSERT INTO menulogs (parent_id, name, date) VALUES ('%ld','%@',julianday('%@'));", (long)parent_id, textField.text, string_date];
+        NSString *insert_sql = [[NSString alloc] initWithFormat: @"INSERT INTO menulogs (parent_id, name, color_tag, date) VALUES ('%ld','%@', '%@',julianday('%@'));", (long)parent_id, textField.text, string_hex, string_date];
         [db executeUpdate:insert_sql];
         if (index == 0) {
             parent_id = (NSInteger)[db lastInsertRowId];
@@ -195,9 +226,9 @@
 }
 
 
-/**
- 日時のボタンが押された際に，DatePickerを立ち上げる
- **/
+//=============================================
+//  日時のボタンが押された際に，DatePickerを立ち上げる
+//=============================================
 - (void)touchDate:(id)sender {
     saveStr = recordDate.currentTitle;
     
@@ -207,17 +238,17 @@
     
     
     // UIDatePicker
-    CGRect pickerFrame = CGRectMake(0, 44, 0, 0);
-    UIDatePicker *viewDatePicker = [[UIDatePicker alloc] initWithFrame:pickerFrame];
+    CGRect          pickerFrame = CGRectMake(0, 44, 0, 0);
+    UIDatePicker    *viewDatePicker = [[UIDatePicker alloc] initWithFrame:pickerFrame];
     [viewDatePicker setDatePickerMode:UIDatePickerModeDate];
     
-    NSDateFormatter *inputDateFormatter = [[NSDateFormatter alloc] init];
-    NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ja_JP"];
+    NSDateFormatter     *inputDateFormatter = [[NSDateFormatter alloc] init];
+    NSLocale            *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ja_JP"];
     [inputDateFormatter setLocale:locale];
     [inputDateFormatter setCalendar:[[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar]];
     [inputDateFormatter setDateFormat:@"yyyy/MM/dd"];
-    NSString *inputDateString = saveStr;
-    NSDate *inputDate = [inputDateFormatter dateFromString:inputDateString];
+    NSString    *inputDateString = saveStr;
+    NSDate      *inputDate = [inputDateFormatter dateFromString:inputDateString];
     [viewDatePicker setDate:inputDate];
     
     // UIDatePicker の値が変化した際
@@ -227,71 +258,158 @@
     [basicSheet addSubview:viewDatePicker];
     
     // UIActionView はモーダルなので，抜け出しボタンを追加
-    UIToolbar *controlToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, basicSheet.bounds.size.width, 44)];
+    UIToolbar   *controlToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, basicSheet.bounds.size.width, 44)];
     [controlToolBar setBarStyle:UIBarStyleBlack];
     [controlToolBar sizeToFit];
     
-    UIBarButtonItem *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
-    UIBarButtonItem *setButton_1 = [[UIBarButtonItem alloc] initWithTitle:@"設定" style:UIBarButtonItemStyleDone target:self action:@selector(dismissSet)];
-    UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithTitle:@"キャンセル" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelSet)];
+    UIBarButtonItem     *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    UIBarButtonItem     *setButton_1 = [[UIBarButtonItem alloc] initWithTitle:@"設定" style:UIBarButtonItemStyleDone target:self action:@selector(dismissSet)];
+    UIBarButtonItem     *cancelButton = [[UIBarButtonItem alloc] initWithTitle:@"キャンセル" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelSet)];
     
     [controlToolBar setItems:[NSArray arrayWithObjects:spacer, setButton_1, cancelButton, nil] animated:NO];
 
     [basicSheet addSubview:controlToolBar];
-
+    
     // show UIActionView
     [basicSheet showInView:self.view];
     [basicSheet setBounds:CGRectMake(0, 0, 320, 520)];
 }
 
-/**
- 選択された日時でdata_valueを上書き
- **/
+//=======================================
+//  選択された日時でdata_valueを上書き
+//=======================================
 - (void)getSelectedTime{
-    NSArray *listOfView = [basicSheet subviews];
+    NSArray     *listOfView = [basicSheet subviews];
     for (UIView *subView in listOfView){
         if ([subView isKindOfClass:[UIDatePicker class]]){
             date_value = [(UIDatePicker *)subView date];
         }
     }
     
-    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    NSDateFormatter     *fmt = [[NSDateFormatter alloc] init];
     [fmt setDateFormat:@"yyyy/MM/dd"];
     [self.recordDate setTitle:[fmt stringFromDate:date_value] forState:UIControlStateNormal];
 }
 
-/**
- 選択された日時を設定したので，ActionViewを閉じる
- **/
+//==============================================
+//  選択された日時を設定したので，ActionViewを閉じる
+//==============================================
 - (void)dismissSet{
     [basicSheet dismissWithClickedButtonIndex:0 animated:YES];
 }
-/**
- キャンセルされた際に，日時設定をもとに戻す
- **/
+//=========================================
+// キャンセルされた際に，日時設定をもとに戻す
+//=========================================
 - (void)cancelSet{
-    NSDateFormatter *inputDateFormatter = [[NSDateFormatter alloc] init];
-    NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ja_JP"];
+    NSDateFormatter     *inputDateFormatter = [[NSDateFormatter alloc] init];
+    NSLocale            *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ja_JP"];
     [inputDateFormatter setLocale:locale];
     [inputDateFormatter setCalendar:[[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar]];
     [inputDateFormatter setDateFormat:@"yyyy/MM/dd"];
-    NSString *inputDateStr = saveStr;
-    NSDate *inputDate = [inputDateFormatter dateFromString:inputDateStr];
+    NSString    *inputDateStr = saveStr;
+    NSDate      *inputDate = [inputDateFormatter dateFromString:inputDateStr];
     date_value = inputDate;
     
     [self.recordDate setTitle:saveStr forState:UIControlStateNormal];
     [basicSheet dismissWithClickedButtonIndex:0 animated:YES];
 }
 
+//======================================
+//  カラータグボタンが押された際の動作
+//======================================
+- (void)touchColorTagButton:(id)sender {
+    // UIActionView setting
+    colorSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:nil cancelButtonTitle:@"" destructiveButtonTitle:nil otherButtonTitles:nil];
+    [colorSheet setActionSheetStyle:UIActionSheetStyleBlackTranslucent];
+    
+    // set delegate
+    colorSheet.delegate = self;
+    
+    // カラーを定義
+    UIColor     *color_array[2][5] = {
+        {[UIColor blueColor], [UIColor orangeColor], [UIColor blackColor], [UIColor greenColor],[UIColor redColor]},
+        {[UIColor cyanColor], [UIColor magentaColor], [UIColor whiteColor], [UIColor yellowColor], [UIColor brownColor]}};
+    
+    // 各種カラーボタン設定
+    double  color_array_col_size = sizeof(color_array[0]) / sizeof(color_array[0][0]);
+    double  color_array_row_size = sizeof(color_array) / sizeof(color_array[0]);
+    int     divide_width = ([[UIScreen mainScreen] bounds].size.width - ACVIEW_PADDING * 2 ) / color_array_col_size ;
+    int     divide_width_padding = divide_width * 0.1;
+    
+    for (int row = 0; row < color_array_row_size; row++) {
+        for (int col = 0; col < color_array_col_size; col++) {
+            CGRect  button_rect = CGRectMake(ACVIEW_PADDING + divide_width * col + divide_width_padding, ACVIEW_TOP + row * 2 * COLOR_HEIGHT, divide_width * 0.8, COLOR_HEIGHT);
+            UIButton    *button = [[UIButton alloc] initWithFrame:button_rect];
+            [[button layer] setCornerRadius: 5];
+            [button setClipsToBounds:YES];
+            [button setBackgroundColor:color_array[row][col]];
+            [button addTarget:self action:@selector(touchColor:) forControlEvents:UIControlEventTouchUpInside];
+            
+            // 枠線表示
+            if (saveColor == color_array[row][col]) {
+                [[button layer] setBorderColor:[[UIColor cyanColor] CGColor]];
+                [[button layer] setBorderWidth:2.0];
+                button.tag = tagColorbutton;
+            }
+            
+            [colorSheet addSubview:button];
+        }
+    }
+    
+    
+    // 設定ボタン
+    UIToolbar   *colorToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, colorSheet.bounds.size.width, 44)];
+    [colorToolBar setBarStyle:UIBarStyleBlack];
+    [colorToolBar sizeToFit];
+    
+    UIBarButtonItem     *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    UIBarButtonItem     *colorSetButton = [[UIBarButtonItem alloc] initWithTitle:@"設定" style:UIBarButtonItemStyleDone target:self action:@selector(colorSet)];
+    UIBarButtonItem     *colorCancelButton = [[UIBarButtonItem alloc] initWithTitle:@"キャンセル" style:UIBarButtonItemStyleBordered target:self action:@selector(colorCancelSet)];
+    [colorToolBar setItems:[NSArray arrayWithObjects:spacer, colorSetButton, colorCancelButton, nil]];
+    
+    [colorSheet addSubview:colorToolBar];
 
-/**
- メニューのカラムを追加する
- **/
+    
+    [colorSheet showInView:self.view];
+    [colorSheet setBounds:CGRectMake(0, 0, 320, 320)];
+}
+
+- (void)touchColor:(UIButton *)button{
+    UIColor *select_color = button.backgroundColor;
+    
+    // 他ボタンの選択解除
+    UIButton *selected_color_button = (UIButton *)[colorSheet viewWithTag:tagColorbutton];
+    selected_color_button.tag = 0;
+    [[selected_color_button layer] setBorderWidth:0];
+    
+    // ボタンを選択状態にする
+    [[button layer] setBorderColor:[[UIColor cyanColor] CGColor]];
+    [[button layer] setBorderWidth:2.0];
+    button.tag = tagColorbutton;
+    
+    [self.colorTagButton setBackgroundColor:select_color];
+}
+
+- (void)colorSet{
+    saveColor = self.colorTagButton.backgroundColor;
+    [colorSheet dismissWithClickedButtonIndex:0 animated:YES];
+    
+}
+
+- (void)colorCancelSet{
+    [self.colorTagButton setBackgroundColor:saveColor];
+    [colorSheet dismissWithClickedButtonIndex:0 animated:YES];
+    
+}
+
+//======================================
+// メニューのカラムを追加する
+//======================================
 - (void)button_Tupped:(id)sender {
    
     // textField 生成
-    CGRect rect = CGRectMake(100, menu_index * 60 + 150, 200, 30);
-    UITextField *customTextField = [[UITextField alloc] initWithFrame:rect];
+    CGRect  rect = CGRectMake(100, menu_index * FIELD_SPAN + TOP_SPAN + FIELD_SPAN * 2, 200, 30);
+    UITextField     *customTextField = [[UITextField alloc] initWithFrame:rect];
 
     customTextField.borderStyle = UITextBorderStyleRoundedRect;
     customTextField.textAlignment = NSTextAlignmentLeft;
@@ -305,13 +423,13 @@
     menu_index++;
     
     // ボタンの位置調節
-    CGRect windowSize = [[UIScreen mainScreen] bounds];
-    [self.addMenuButton setCenter:CGPointMake(windowSize.size.width / 2.0, menu_index * 60 + 165)];
+    CGRect  windowSize = [[UIScreen mainScreen] bounds];
+    [self.addMenuButton setCenter:CGPointMake(windowSize.size.width / 2.0, menu_index * FIELD_SPAN + TOP_SPAN + FIELD_SPAN * 2 + 15)];
     
     // scroll view 幅調節
-    if (menu_index * 60 + 165 + 180 > self.view.bounds.size.height) {
-        scrollView.contentSize = CGSizeMake(self.view.bounds.size.width, menu_index * 60 + 165 + 180);
-        [self.submitButton setCenter:CGPointMake(windowSize.size.width / 2.0, scrollView.contentSize.height - 80)];
+    if (menu_index * FIELD_SPAN + TOP_SPAN + FIELD_SPAN * 2 + 15 + 100 + BOTTOM_SPAN > self.view.bounds.size.height) {
+        scrollView.contentSize = CGSizeMake(self.view.bounds.size.width, menu_index * FIELD_SPAN + TOP_SPAN + FIELD_SPAN * 2 + 15 + BOTTOM_SPAN + 100);
+        [self.submitButton setCenter:CGPointMake(windowSize.size.width / 2.0, scrollView.contentSize.height - BOTTOM_SPAN)];
     }
 }
 
