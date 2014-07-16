ff19efd89cced3a672431425f7cb5db5721ddae0
akira <h3.poteto@gmail.com>
2014-07-16 16:23:24 +0900

    実機デバッグ用にDBのpath調節

--- MenuRecords2/LocalDBClient.m
+++ MenuRecords2/LocalDBClient.m
@@ -16,9 +16,22 @@
 {
     self = [super init];
     if (self) {
-        NSArray     *paths = NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES);
-        NSString    *dir = [paths objectAtIndex:0];
-        _dbPath = [dir stringByAppendingPathComponent:@"menu_records.db"];
+        NSArray     *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        
+        if ([paths count] >= 1) {
+            NSString    *dir = [paths objectAtIndex:0];
+            _dbPath = [dir stringByAppendingPathComponent:@"menu_records.db"];
+        } else {
+            NSLog(@"search Document path error. database file open error.");
+            return false;
+        }
+        
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        if (![fileManager fileExistsAtPath:_dbPath]) {
+            // DBファイルが存在しないなら作ればいい
+            [fileManager createFileAtPath:_dbPath contents:nil attributes:nil];
+        }
+        
         FMDatabase  *db = [FMDatabase databaseWithPath:_dbPath];
         NSString    *sql = @"CREATE TABLE IF NOT EXISTS menulogs (id INTEGER PRIMARY KEY AUTOINCREMENT, parent_id INTEGER, name TEXT, color_tag TEXT, original_id INTEGER ,date REAL, sync BOOLEAN);";
         [db open];
